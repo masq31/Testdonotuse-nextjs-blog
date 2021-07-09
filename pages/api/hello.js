1b94f616f291ace819b01fcdf0bb6ea3b20c72fd
Theguesst <bojo109@protonmail.com>
2021-07-09 18:25:05 -0400

    Allow get only

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,14 +1,20 @@
 import next from "next"
 import image from "next/image"
-import Date from '../components/date'
+import { resetWarningCache } from "prop-types"
+//import Date from '../components/date'
 
 export default (req, res) => {
-  res.status(200).json({ 
-    ID: 1,
+  if (req.method === 'GET') {
+    res.status(200).json({ 
+    //ID: 1,
     hello: 'Hello', 
     goodbye: 'Goodbye',
     image: 'placeholder', //src="https://site.com/public/image/$"{(setting)},
     date2: 'placeholder' //<Date dateString={date} />
                 }
               )
+            }
+        else {null
+        }
+      
 }
