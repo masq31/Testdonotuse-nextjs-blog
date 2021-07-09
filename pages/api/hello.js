ba968333b2b1d0f8f28eafaa10b0804b6c92026f
Theguesst <bojo109@protonmail.com>
2021-07-09 18:10:54 -0400

    Testing API modification. USe with resource file

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,14 @@
+import next from "next"
+import image from "next/image"
+import Date from '../components/date'
+
 export default (req, res) => {
-  res.status(200).json({ text: 'Hello' })
+  res.status(200).json({ 
+    ID: 1,
+    hello: 'Hello', 
+    goodbye: 'Goodbye',
+    image: 'placeholder', //src="https://site.com/public/image/$"{(setting)},
+    date2: 'placeholder' //<Date dateString={date} />
+                }
+              )
 }
